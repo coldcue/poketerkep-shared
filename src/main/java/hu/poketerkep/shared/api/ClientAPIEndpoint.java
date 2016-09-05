6adaad9db3eed2a8fbcd99ad979036884fd9927f
Andras Szell <andras.szell@doclerholding.com>
2016-09-05 19:37:32 +0200

    nextScanLocations changed

--- src/main/java/hu/poketerkep/shared/api/ClientAPIEndpoint.java
+++ src/main/java/hu/poketerkep/shared/api/ClientAPIEndpoint.java
@@ -4,10 +4,8 @@ import hu.poketerkep.shared.geo.Coordinate;
 import hu.poketerkep.shared.model.Pokemon;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Collection;
-
 public interface ClientAPIEndpoint {
     ResponseEntity<Void> addPokemons(Pokemon[] pokemons);
 
-    ResponseEntity<Collection<Coordinate>> nextScanLocations(int limit);
+    ResponseEntity<Coordinate[]> nextScanLocations(int limit);
 }

a38b19288646fcf5ce8b5fcf73d5133ac6807e01
Andras Szell <coldcue@gmail.com>
2016-09-07 22:56:50 +0200

    Some tests and ScanCoordinatesService.java added

--- src/main/java/hu/poketerkep/shared/config/Constants.java
+++ src/main/java/hu/poketerkep/shared/config/Constants.java
@@ -1,7 +1,9 @@
 package hu.poketerkep.shared.config;
 
 public class Constants {
-    public static final int UNUSED_USER_TIME_SECONDS = 4 * 60 * 60; // 4 hours
+    public static final int UNUSED_USER_TIME_SECONDS = 8 * 60 * 60; // 8 hours
+    public static final long MAX_USED_USER_TIME_SECONDS = 4 * 60 * 60; // 4 hours
+
     public static final long PRIORITY_TIME_MINS = 15;
-    public static final long SCAN_DELAY = 10;
+    public static final long SCAN_DELAY = 15;
 }

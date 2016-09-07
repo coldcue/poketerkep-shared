1b7cc08703cf2b2246ea6c0d60b17ed2130bef03
Andras Szell <coldcue@gmail.com>
2016-09-07 20:22:55 +0200

    constants moved to shared

---
+++ src/main/java/hu/poketerkep/shared/config/Constants.java
@@ -0,0 +1,6 @@
+package hu.poketerkep.shared.config;
+
+public class Constants {
+    public static final int UNUSED_USER_TIME_SECONDS = 4 * 60 * 60; // 4 hours
+    public static final long PRIORITY_TIME_MINS = 15;
+}

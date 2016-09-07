1316f39e335665b887be3ebac8b47c4d74b5b573
Andras Szell <coldcue@gmail.com>
2016-09-07 21:03:26 +0200

    MapPokemonMapperTest.java added

--- src/main/java/hu/poketerkep/shared/config/Constants.java
+++ src/main/java/hu/poketerkep/shared/config/Constants.java
@@ -3,4 +3,5 @@ package hu.poketerkep.shared.config;
 public class Constants {
     public static final int UNUSED_USER_TIME_SECONDS = 4 * 60 * 60; // 4 hours
     public static final long PRIORITY_TIME_MINS = 15;
+    public static final long SCAN_DELAY = 10;
 }

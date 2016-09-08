3048fdc7814bc400c9f2eb8b72e5468e8a5b06e4
Andras Szell <coldcue@gmail.com>
2016-09-08 16:29:17 +0200

    Constants changed

--- src/main/java/hu/poketerkep/shared/config/Constants.java
+++ src/main/java/hu/poketerkep/shared/config/Constants.java
@@ -4,6 +4,8 @@ public class Constants {
     public static final int UNUSED_USER_TIME_SECONDS = 8 * 60 * 60; // 8 hours
     public static final long MAX_USED_USER_TIME_SECONDS = 4 * 60 * 60; // 4 hours
 
-    public static final long PRIORITY_TIME_MINS = 15;
-    public static final long SCAN_DELAY = 15;
+    public static final long PRIORITY_TIME_MINS = 5;
+    public static final long SCAN_DELAY = 20;
+    public static final int CLIENTS_PER_TOR = 5;
+    public static final int CLIENTS_PER_THREAD = 5;
 }

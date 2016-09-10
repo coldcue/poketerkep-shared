f805fc46132cbb85d360caa754da10eacb383018
Andras Szell <coldcue@gmail.com>
2016-09-10 11:36:11 +0200

    Rivers Health check added

--- src/main/java/hu/poketerkep/shared/config/Constants.java
+++ src/main/java/hu/poketerkep/shared/config/Constants.java
@@ -2,10 +2,6 @@ package hu.poketerkep.shared.config;
 
 public class Constants {
     public static final int UNUSED_USER_TIME_SECONDS = 8 * 60 * 60; // 8 hours
-    public static final long MAX_USED_USER_TIME_SECONDS = 4 * 60 * 60; // 4 hours
 
     public static final long PRIORITY_TIME_MINS = 5;
-    public static final long SCAN_DELAY = 20;
-    public static final int CLIENTS_PER_TOR = 5;
-    public static final int CLIENTS_PER_THREAD = 5;
 }
